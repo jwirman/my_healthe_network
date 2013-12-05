51904420abb0e52d4e30ff7d4b6fa847ecaea0d5
jwirman <jwirman@gmail.com>
2013-12-04 21:38:04 -0600

    tweak validations on number_doses

--- app/models/users_med.rb
+++ app/models/users_med.rb
@@ -3,6 +3,7 @@ class UsersMed < ActiveRecord::Base
   belongs_to :med
 
   validates :med_id, :freq, :start, presence: true, allow_blank: false
-  validates :num_per_dose, :num_doses, numericality: { greater_than: 0, only_integer: true }
+  validates :num_per_dose, numericality: { greater_than: 0, only_integer: true }, allow_blank: true
+  validates :num_doses,    numericality: { greater_than: 0, only_integer: true }, presence: true
 
 end
