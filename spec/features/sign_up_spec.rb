5aca3b7cffeaf7c280a6175c48f27377c6d14fe0
jwirman <jwirman@gmail.com>
2013-12-12 23:26:06 -0600

    spec fixes for changes to simple_form for devise views and addition of confirmable module

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -10,16 +10,16 @@ end
 feature 'user signs up' do
   scenario 'with valid email and password' do
     sign_up_with 'valid@example.com', 'password'
-    expect(page).to have_content('Welcome! You have signed up successfully.')
+    expect(page).to have_content('A message with a confirmation link has been sent to your email address.')
   end
 
   scenario 'with invalid email' do
     sign_up_with 'invalid_email', 'password'
-    expect(page).to have_content('Email is invalid')
+    expect(page).to have_content('is invalid')
   end
 
   scenario 'with blank password' do
     sign_up_with 'valid@example.com', ''
-    expect(page).to have_content("Password can't be blank")
+    expect(page).to have_content("can't be blank")
   end
 end
--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -5,13 +5,14 @@ module Features
       fill_in 'Email', with: email
       fill_in 'First name', with: 'Sign_up_first'
       fill_in 'Last name', with: 'Sign_up_last'
-      fill_in 'Password', with: password
-      fill_in 'Password confirmation', with: password
+      fill_in 'user_password', with: password
+      fill_in 'user_password_confirmation', with: password
       click_button 'Sign up'
     end
 
     def sign_in
       user = create(:user)
+      user.confirm!
       visit new_user_session_path
       fill_in 'Email', with: user.email
       fill_in 'Password', with: user.password
