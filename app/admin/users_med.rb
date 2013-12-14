b0b2d605a4c81652663c76143e294c02b229ec82
jwirman <jwirman@gmail.com>
2013-12-13 21:19:11 -0600

    allow users_med params from active_admin

--- app/admin/users_med.rb
+++ app/admin/users_med.rb
@@ -8,4 +8,13 @@ ActiveAdmin.register UsersMed do
     default_actions
   end
 
+  controller do
+    def permitted_params
+      params.permit users_med: [:user_id, :med_id, :freq, :freq_unit,
+                                :num_per_dose, :start, :first_dose,
+                                :second_dose, :third_dose, :fourth_dose,
+                                :fifth_dose, :sixth_dose, :window, :num_doses]
+    end
+  end
+
 end
